19f69280e045f9abda2852d02a9e0feaacb6f0a7
Matheus Dario <dario.matheus97@gmail.com>
2024-01-10 19:18:55 -0300

    cleaning app/index

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,14 +1,8 @@
-/* eslint-disable no-unused-vars */
-import { useEffect, useRef, useState } from 'react';
 import Head from 'next/head';
 import { Base } from '../Base';
-import { mockBase } from '../Base/mock';
-import { mapData } from '../../api/map-data';
-import * as Styled from './styles';
 import P from 'prop-types';
 import { GridTwoColumns } from '../../components/GridTwoColumns';
 import { PageNotFound } from '../PageNotFound';
-import { Loading } from '../Loading';
 import { GridContent } from '../../components/GridContent';
 import { GridSection } from '../../components/GridSection';
 import { GridImage } from '../../components/GridImage';
@@ -36,7 +30,6 @@ export const App = ({ data }) => {
       {sections.map((section, index) => {
         const { component } = section;
         const key = `${slug}-${index}`;
-        //console.log(component);
 
         if (component === 'section.section-two-columns') {
           return <GridTwoColumns key={key} {...section} />;
