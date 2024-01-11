fea4f0264792e82c64d568d60faa82064b069e5d
Matheus Dario <dario.matheus97@gmail.com>
2024-01-11 20:36:22 -0300

    slug page

---
+++ src/pages/[slug].jsx
@@ -0,0 +1,58 @@
+import { loadPages } from '@/api/load-pages';
+import config from '@/config';
+import { App } from '@/templates/App';
+import P from 'prop-types';
+
+export default function Page({ data }) {
+  return <App data={data} />;
+}
+
+Page.propTypes = {
+  data: P.object,
+};
+
+export const getStaticPaths = async () => {
+  const url = config.url;
+  const raw = await fetch(url);
+  const json = await raw.json();
+  const data = json.data;
+
+  const mySlug = data.map((page) => {
+    const {
+      attributes: { slug },
+    } = page;
+
+    return { slug };
+  });
+
+  console.log(mySlug[0].slug);
+  return {
+    paths: [
+      {
+        params: { slug: mySlug[0].slug },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  let data = null;
+
+  try {
+    data = await loadPages(params.slug);
+  } catch (e) {
+    data = null;
+  }
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      data,
+    },
+  };
+};
